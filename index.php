e63907707fbd968da3e6e71d900d27d2229a8e7e
emilianogastaldo <emiliano0303@gmail.com>
2024-02-20 15:25:31 +0100

    printed hotels names

--- index.php
+++ index.php
@@ -3,3 +3,20 @@ include 'data.php';
 
 var_dump($hotels)
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <h2>Hotels</h2>
+        <?php foreach($hotels as $hotel) :?>
+            <p><?= $hotel['name']?></p>
+        <?php endforeach?>
+    </section>
+</body>
+</html>
